4ab07f380f4569d1975bb1a3dc69894077ca7e6f
Genjo <gena.1088@bk.ru>
2019-08-06 17:08:31 +0300

    task 1.2.2.done

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -3,7 +3,7 @@ $fName = mb_substr(mb_strtoupper($_POST['firstName'],'utf-8'),0,1,'utf-8').mb_su
 $lName = mb_substr(mb_strtoupper($_POST['lastName'],'utf-8'),0,1,'utf-8').mb_substr($_POST['lastName'],1,mb_strlen($_POST['lastName'],'utf-8'),'utf-8');
 $mName = mb_substr(mb_strtoupper($_POST['middleName'],'utf-8'),0,1,'utf-8').mb_substr($_POST['middleName'],1,mb_strlen($_POST['middleName'],'utf-8'),'utf-8');
 $fullName = mb_ucfirst($fName . "" . $mName . " " . $lName);
-$fio;
-$surnameAndInitials;
+$fio =substr($fName,0, 1) .".". substr($mName, 0, 1).".". substr($lName, 0, 1);
+$surnameAndInitials = $fName ."". substr($mName, 0, 1).".". substr($lName, 0, 1);
 
 ?>
