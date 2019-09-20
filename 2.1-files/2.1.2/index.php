45533d9037305239133c69c3a81484c8b349892e
Genjo <gena.1088@bk.ru>
2019-09-20 17:10:07 +0300

    taks 2.1.2-done

---
+++ 2.1-files/2.1.2/index.php
@@ -0,0 +1,23 @@
+<?php
+
+function uploadImage($image) {
+    $name = $image['name'];
+    $tmpName = $image['tmp_name'];
+    foreach($_FILES['image']['error']as$key=>$error){
+        if($error==UPLOAD_ERR_OK){
+            $tmpName=$_FILES['image']['tmp_name'][$key];
+            if(is_uploaded_file($tmpName)){
+                $pathParts=pathinfo($_FILES['image']['name'][$key]);
+                if($pathParts['extension']!='php'){
+                    move_uploaded_file($tmpName, "/uploads".'/'.$pathParts['basename']);
+                }
+            }
+        }
+    }      
+move_uploaded_file($tmpName, "/uploads" . $name);   
+};
+
+uploadImage($_FILES['image']);
+
+                                                                           
+?>
