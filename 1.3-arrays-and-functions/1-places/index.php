5ab9598088b0a62d96dd9639f12901b64f89902e
Genjo <gena.1088@bk.ru>
2019-10-30 17:17:21 +0300

    obnovil

--- 1.3-arrays-and-functions/1-places/index.php
+++ 1.3-arrays-and-functions/1-places/index.php
@@ -1,29 +1,38 @@
 <?php
-
-function generate($rows, $placesPerRow, $avaliableCount) {  
-    $requiredPlace = 5;  
-    return $arr[[]] = false;
-    if ($avaliableCount < $requiredPlace){
-        return false;
-    }};
-
-function reserve($map, $row, $place){
-
-};
-
-
+function generate($rows, $placesPerRow, $avaliableCount) {
+    if ($rows * $placesPerRow  > $avaliableCount) {
+        $result = FALSE;
+    } else {
+        $result = array(array());
+        for ($i = 0; $i <= $rows-1; $i++) {
+            for ($y = 0; $y <= $placesPerRow-1; $y++) {
+                $result[$i][$y] = FALSE;
+            
+            }
+        }
+    }
+return $result;
+}
+function reserve($map, $row, $place) {
+  
+    if ($map[$row-1][$place-1] == FALSE) {
+        $map[$row-1][$place-1] = TRUE;
+        $result = TRUE;
+    } else {
+        $result = FALSE;
+    }
+    
+return $result;
+}    
 $chairs = 50;
 $map = generate(5, 8, $chairs);
-$requiredRow = 3;
-$requiredPlace = 5;
-
-$reverve = reserve($map, $requiredRow, $requiredPlace);
-logReserve($requiredRow, $requiredPlace, $reverve);
-
-$reverve = reserve($map, $requiredRow, $requiredPlace);
-logReserve($requiredRow, $requiredPlace, $reverve);
-
-
+$requireRow = 3;
+$requirePlace = 5;
+$reverve = reserve($map, $requireRow, $requirePlace);
+logReserve($requireRow, $requirePlace, $reverve);
+$map[$requireRow-1][$requirePlace-1]=$reverve;
+$reverve = reserve($map, $requireRow, $requirePlace);
+logReserve($requireRow, $requirePlace, $reverve);
 function logReserve($row, $place, $result){
     if ($result) {
         echo "Ваше место забронировано! Ряд $row, место $place".PHP_EOL;
@@ -31,5 +40,4 @@ function logReserve($row, $place, $result){
         echo "Что-то пошло не так=( Бронь не удалась".PHP_EOL;
     }
 }
-
-  ?>
+?>  
