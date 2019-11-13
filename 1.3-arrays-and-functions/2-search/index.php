9a74cc7f95426199c64c460d28ca7a77c2e2d751
Genjo <gena.1088@bk.ru>
2019-11-13 20:26:23 +0300

    peredelal

--- 1.3-arrays-and-functions/2-search/index.php
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -1,60 +1,59 @@
-<?php
-function generate($rows,$placesPerRow,$avaliableCount) {
-    $map = [];
-    if ($rows*$placesPerRow > $avaliableCount) {
-        return FALSE;
-    }else {
-        for($i = 0; $i < $rows; $i++) {
-            $map[$i] = [];
-            for($j = 0; $j < $placesPerRow; $j++) {
-                $map[$i][] = FALSE;
-            }
-        }
-        return $map;
+  
+<?php 
+  function generate($rows, $placesPerRow, $avaliableCount) {
+    if ($rows * $placesPerRow > $avaliableCount) return false;
+  
+    $msp = [];
+  
+    for ($i=0; $i <  $rows; $i++) {
+      for ($j=0; $j < $placesPerRow; $j++) {
+        $map[$i][$j] = false;
+      }
     }
-}
-function reserve(&$map,$row,$place) {
-    if($map[$row-1][$place-1] === FALSE) {
-        $map[$row-1][$place-1] = TRUE;
-        return TRUE;
-    }else {
-        return FALSE;
-    }
-}
-$chairs = 50;
-$map = generate(5, 8, $chairs);
-$requireRow = 3;
-$requiredPlace = 5;
-$reverve = reserve($map, $requireRow, $requiredPlace);
-logReserve($requireRow, $requiredPlace, $reverve);
-$reverve = reserve($map, $requireRow, $requiredPlace);
-logReserve($requireRow, $requiredPlace, $reverve);
-function logReserve($row, $place, $result){
+ 
+    return $map;
+  }
+  
+  function reserve(&$map, $row, $place) {
+    if ($map[$row-1][$place-1] === false) {
+      $map[$row-1][$place-1] = true;
+      return true;
+    } else return false;
+  }
+  
+  function logReserve($row, $place, $result){
     if ($result) {
         echo "Ваше место забронировано! Ряд $row, место $place".PHP_EOL;
+        echo '<br>';
     } else {
-        echo "Что-то пошло не так=( Бронь не удалась".PHP_EOL;
+      echo "Что-то пошло не так=( Бронь не удалась".PHP_EOL;
+      echo '<br>';
     }
-}
-function checkPlacesOfMap($map,$requireNearPlaces) {
-    for($i=0;$i<count($map);$i++){
-        $checkPos = 0;
-        $row = $i+1;
-        $places = ["Ряд $row"];        
-        for($j=0;$j<(count($map[$i])-$requireNearPlaces + 1);$j++) {
-            if($checkPos !== $requireNearPlaces) {
-                if($map[$i][$j] === FALSE) {
-                    $checkPos++;
-                    $place = $j+1;
-                    $places[] =  "Место $place"; 
-                } else {
-                    $checkPos = 0;
-                }
-            }else {
-                return $places;
-            }
-        }     
+  }
+  
+  $chairs = 50;
+  $map = generate(5, 8, $chairs);
+  $requireRow = 1;
+  $requirePlace = 4;
+  $result = reserve($map, $requireRow, $requirePlace);
+  logReserve($requireRow, $requirePlace, $result);
+  function checkPlacesOfMap($map,$requireNearPlaces) {
+    $nearPlacesCount = 0;
+    for ($i=0; $i < count($map); $i++) {
+      for ($j=0; $j < (count($map[$i]) - $requireNearPlaces + 1); $j++) {
+        $nearPlacesCount = 0;
+        for ($k=0; $k < $requireNearPlaces; $k++) {
+          if ($map[$i][$j+$k] === false) {$nearPlacesCount++;}
+        }
+        if ($nearPlacesCount == $requireNearPlaces) {
+          $roww = $i+1;
+          $placeStart = $j+1;
+          $placeEnd = $placeStart + $requireNearPlaces -1;
+          return "<br> Ряд $roww Места $placeStart-$placeEnd";
+        }
+      }
     }
-}
-print_r(checkPlacesOfMap($map,2));
+    return  "Нет";
+  }
+  echo checkPlacesOfMap($map,5);
 ?>
